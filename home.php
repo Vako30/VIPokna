4337890cc67d9a3e59abe734343fd7b2bffede56
Smarter1 <vako3008@gmail.com>
2014-12-16 20:09:14 +0400

    Initial Commit

--- home.php
+++ home.php
@@ -1,1 +1,212 @@
-Hello
+<div id="page">
+
+<div id="columns">
+<div id="mainContent">
+
+<div class="local-tasks"></div>
+
+
+<div id="content">
+
+    <div class="region region-content">
+        <div id="block-views-slides_front_page-block" class="block block-views">
+            <div class="block-inner">
+
+
+                <div class="content">
+                    <div
+                        class="view view-slides-front-page view-id-slides_front_page view-display-id-block view-dom-id-d359d14af50fc96ac3ad43ed160234f5">
+
+
+                        <div class="view-content">
+                            <div class="views-row views-row-1 views-row-odd views-row-first">
+
+                                <div class="views-field views-field-field-slide-bg">
+                                    <div class="field-content"><img typeof="foaf:Image"
+                                                                    src="img/retro_dra_69.jpg"
+                                                                    width="1600" height="1200" alt=""/>
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-field-slide-badge">
+                                    <div class="field-content"></div>
+                                </div>
+                                <div class="views-field views-field-title"><span
+                                        class="field-content"><a
+                                            href="/content/slayd6">Слайд6</a></span></div>
+                                <div class="views-field views-field-field-slide-description">
+                                    <div class="field-content">Здесь небольшой текст о данном виде
+                                        продукции на несколько строчек
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-field-slide-link">
+                                    <div class="field-content"><a href="http://vipokna63.ru/"
+                                                                  class="slide_link">ПОДРОБНЕЕ</a></div>
+                                </div>
+                            </div>
+                            <div class="views-row views-row-2 views-row-even">
+
+                                <div class="views-field views-field-field-slide-bg">
+                                    <div class="field-content"><img typeof="foaf:Image"
+                                                                    src="img/slide1.jpg"
+                                                                    alt=""/></div>
+                                </div>
+                                <div class="views-field views-field-field-slide-badge">
+                                    <div class="field-content"><img typeof="foaf:Image"
+                                                                    src="img/slide3.png"
+                                                                    width="146" height="146" alt=""/>
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-title"><span
+                                        class="field-content"><a href="/content/testovyy-slayd">ТЕСТОВЫЙ
+                                            СЛАЙД</a></span></div>
+                                <div class="views-field views-field-field-slide-description">
+                                    <div class="field-content">Здесь небольшой текст о данном виде
+                                        продукции на несколько строчек
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-field-slide-link">
+                                    <div class="field-content"><a href="http://vipokna63.ru/"
+                                                                  class="slide_link">ПОДРОБНЕЕ</a></div>
+                                </div>
+                            </div>
+                            <div class="views-row views-row-3 views-row-odd">
+
+                                <div class="views-field views-field-field-slide-bg">
+                                    <div class="field-content"><img typeof="foaf:Image"
+                                                                    src="img/slide1.jpg"
+                                                                    alt=""/></div>
+                                </div>
+                                <div class="views-field views-field-field-slide-badge">
+                                    <div class="field-content"><img typeof="foaf:Image"
+                                                                    src="img/slide1.png"
+                                                                    width="146" height="146" alt=""/>
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-title"><span
+                                        class="field-content"><a href="/content/nasha-produkciya">Наша
+                                            продукция</a></span></div>
+                                <div class="views-field views-field-field-slide-description">
+                                    <div class="field-content">Здесь небольшой текст о данном виде
+                                        продукции на несколько строчек
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-field-slide-link">
+                                    <div class="field-content"><a href="http://vipokna63.ru/"
+                                                                  class="slide_link">ПОДРОБНЕЕ</a></div>
+                                </div>
+                            </div>
+                            <div class="views-row views-row-4 views-row-even views-row-last">
+
+                                <div class="views-field views-field-field-slide-bg">
+                                    <div class="field-content"><img typeof="foaf:Image"
+                                                                    src="img/slide1.jpg"
+                                                                    alt=""/></div>
+                                </div>
+                                <div class="views-field views-field-field-slide-badge">
+                                    <div class="field-content"><img typeof="foaf:Image"
+                                                                    src="img/slide2.png"
+                                                                    width="146" height="146" alt=""/>
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-title"><span
+                                        class="field-content"><a href="/content/plastikovye-okna">Пластиковые
+                                            окна</a></span></div>
+                                <div class="views-field views-field-field-slide-description">
+                                    <div class="field-content">Здесь небольшой текст о данном виде
+                                        продукции на несколько строчек
+                                    </div>
+                                </div>
+                                <div class="views-field views-field-field-slide-link">
+                                    <div class="field-content"><a href="http://vipokna63.ru/"
+                                                                  class="slide_link">ПОДРОБНЕЕ</a></div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div id="block-system-main" class="block block-system">
+            <div class="block-inner">
+
+
+                <div class="content">
+                    <div id="node-8" class="node node-page" about="/main" typeof="foaf:Document">
+                        <div class="node-inner clearfix">
+
+
+                            <div class="node-content">
+                                <div class="field field-name-body field-type-text-with-summary field-label-hidden">
+                                    <div class="field-item even" property="content:encoded">
+                                        <div class="out out-pattern" style="opacity: 1;">
+                                            <p> </p>
+                                        </div>
+                                        <p> </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="node-links">
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div id="block-webform-client-block-8" class="block block-webform">
+            <div class="block-inner">
+
+
+                <div class="content">
+                    <form class="webform-client-form" enctype="multipart/form-data" action="/main"
+                          method="post" id="webform-client-form-8--2" accept-charset="UTF-8">
+                        <div>
+                            <div class="form-item webform-component webform-component-textfield"
+                                 id="webform-component-name">
+                                <input type="text" id="edit-submitted-name--2" name="submitted[name]"
+                                       value="" size="60" maxlength="128" class="form-text"/>
+                            </div>
+                            <div class="form-item webform-component webform-component-textfield"
+                                 id="webform-component-email">
+                                <input type="text" id="edit-submitted-email--2" name="submitted[email]"
+                                       value="" size="60" maxlength="128" class="form-text required"/>
+                            </div>
+                            <div class="form-item webform-component webform-component-textarea"
+                                 id="webform-component-message">
+                                <div class="form-textarea-wrapper"><textarea
+                                        id="edit-submitted-message--2" name="submitted[message]"
+                                        cols="60" rows="5" class="form-textarea"></textarea></div>
+                            </div>
+                            <input type="hidden" name="details[sid]" value=""/>
+                            <input type="hidden" name="details[page_num]" value="1"/>
+                            <input type="hidden" name="details[page_count]" value="1"/>
+                            <input type="hidden" name="details[finished]" value="0"/>
+                            <input type="hidden" name="form_build_id"
+                                   value="form-688NUlQZ_9TDM1V2ZpLJAAqT_64GUU0V3uAEfsO_fKE"/>
+                            <input type="hidden" name="form_id" value="webform_client_form_8"/>
+
+                            <div class="form-actions form-wrapper" id="edit-actions--2"><input
+                                    type="submit" id="edit-submit--2" name="op" value="Отправить"
+                                    class="form-submit"/></div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
+<!-- /main-content -->
+</div>
+<!-- /columns -->
+
+</div>
+<!-- /page -->
