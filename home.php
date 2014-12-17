cc99d62148ec3f9786b98c096c32f8d54ed233db
Smart <petrosyan.vahe@rambler.ru>
2014-12-17 15:55:31 +0400

    secondary content added to home.php

--- home.php
+++ home.php
@@ -199,12 +199,101 @@
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 <!-- /main-content -->
 </div>
 <!-- /columns -->
+    <div id="secondaryContent">
 
+        <div class="region region-secondary-content">
+            <div id="block-block-3" class="block block-block">
+                <div class="block-inner">
+
+
+                    <div class="content">
+                        <div id="secondaryPage">
+                            <div class="box">
+                                <div class="box">
+                                    <div class="box">
+                                        <div class="box">
+                                            <p> </p>
+
+                                            <p class="rtecenter"><span
+                                                    style="font-family:arial,helvetica,sans-serif;"><span
+                                                        style="font-size:14px;">О КОМПАНИИ</span></span></p>
+
+                                            <p class="rtecenter"><span
+                                                    style="font-family:arial,helvetica,sans-serif;"><span
+                                                        style="font-size:14px;">«VIP-ОКНА» — это производитель окон, который начал свое развитие в 1993 в Армении, и два года назад начали свою деятельность в городе Самара.</span></span><span
+                                                    style="font-family: arial, helvetica, sans-serif; font-size: 14px;"> За это время мы успели заслужить доверие и уважение многих клиентов. Мы работаем качественно, выполняя даже самые сложные заказы точно в срок.</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="rtecenter">
+                             
+                        </div>
+                        <div class="rtecenter">
+                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">Пластиковые окна на сегодняшний день являются незаменимым элементом дизайнерских решений, приносящим в дом солнечный свет,</span></span><span
+                                style="font-size: 14px; font-family: arial, helvetica, sans-serif;">тепло и уют.</span>
+                        </div>
+                        <div class="rtecenter">
+                            <span style="font-size: 14px; font-family: arial, helvetica, sans-serif;">На рынке оконной индустрии представлен широкий выбор разноплановых оконных конструкций.</span>
+                        </div>
+                        <div class="rtecenter">
+                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">Мы не устанавливаем дешевых окон.</span></span>
+                        </div>
+                        <div class="rtecenter">
+                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">Только надёжные, проверенные марки, но по приемлемым ценам. И не стоит забывать о технологии установки окон ПВХ - неправильный монтаж </span></span>
+                        </div>
+                        <div class="rtecenter">
+                            <span style="font-family:arial,helvetica,sans-serif;"><span style="font-size:14px;">сокращает срок службы даже идеально изготовленного окна. </span></span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div id="block-block-1" class="block block-block">
+                <div class="block-inner">
+
+
+                    <div class="content">
+                        <div class="box alone">
+                            <div class="what1">
+                                <p><span style="font-size:18px;"><strong><span
+                                                style="color:#ff0000;">VIP-OKNA</span></strong><span
+                                            style="color:#000000;"><strong> </strong>- это окна, которые прослужат Вам долгие годы.</span></span>
+                                </p>
+
+                                <div>
+                                    <img alt="" src="img/heart.png"/><span style="font-size:14px;"><span
+                                            style="font-family:arial,helvetica,sans-serif;"><span
+                                                style="color:#ff0000;"><strong><span class="h1">КАЧЕСТВО</span> </strong></span><span
+                                                class="text"> Гварантия высочайшего качества нашей продукции.</span></span></span>
+                                </div>
+                            </div>
+                            <div class="what2">
+                                <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;"><img
+                                            alt="" src="img/calcul.png"/><span style="color:#000080;"><strong><span
+                                                    class="h1">ЦЕНА</span></strong> </span>Приемлимые цены, и скидки для постоянных клиентов.</span></span>
+                            </div>
+                            <div class="what3">
+                                <span style="font-size:14px;"><span style="font-family:arial,helvetica,sans-serif;"><img
+                                            alt="" src="img/clock.png"/><strong> <span style="color:#006400;"><span
+                                                    class="h1">СЖАТЫЕ СРОКИ</span></span></strong><span
+                                            style="color:#006400;"> </span>Выполнение работ любой сложности в сжатые сроки.</span></span>
+                            </div>
+                        </div>
+                        <p> </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
 <!-- /page -->
